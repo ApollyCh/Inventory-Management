d9e7dfba3dbfaee26364558dd0bebdb54cfb7713
1kkiren <1kkiren@mail.ru>
2023-07-15 16:55:48 +0300

    [InventoryLog begin]

---
+++ src/views/InventoryLogPage.vue
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import TopPanel from "@/components/TopPanel.vue";
+
+    export default {
+        name: "InventoryLogPage",
+        components: {
+            TopPanel
+        }
+    }
+</script>
+
+<template>
+    <TopPanel></TopPanel>
+</template>
