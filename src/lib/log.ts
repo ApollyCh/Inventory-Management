0f0e5a9bb5b69e3382b5131a78478c28aaa0ba0f
1kkiren <1kkiren@mail.ru>
2023-07-24 23:02:32 +0300

    [Update] prettier

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,10 +1,10 @@
 export interface Log {
-    id: string,
-    name: string,
-    date: string,
-    time: string,
-    itemId: string,
-    countChange: number,
-    timestamp: number,
-    imageItemUrl: string,
+  id: string
+  name: string
+  date: string
+  time: string
+  itemId: string
+  countChange: number
+  timestamp: number
+  imageItemUrl: string
 }
